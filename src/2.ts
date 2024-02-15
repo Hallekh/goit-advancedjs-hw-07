f62082b74e49543074274a02c78a5d29f6dfa9c0
hallekh <lekhnovska.halyna@gmail.com>
2024-02-15 19:35:33 +0100

    task1-4

--- src/2.ts
+++ src/2.ts
@@ -16,9 +16,9 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
+  public name: string;
+  protected department: string;
+  protected salary: number;
 
   constructor(name: string, department: string, salary: number) {
     this.name = name;
@@ -33,7 +33,10 @@ class Employee {
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary + 10000);
+  }
 }
-
-
+const manager = new Manager("John", "Management", 50000);
+console.log(manager.getEmployeeDetails());
 export {};
